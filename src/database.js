53fc278972686067487a5cf8f1130bead4da3844
Laged <parkkila.matti@gmail.com>
2016-11-13 12:33:45 +0200

    Add new database integration

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const massive = require('massive');
-const connectionString = "postgres://postgres:asdasd@localhost/ticket";
+const connectionString = process.env.DATABASE_URL;
 const db = massive.connectSync({connectionString:connectionString});
 
 module.exports = {
   debug(callback) {
     console.log('debug');
-    db.run('SELECT * FROM ticketsunfiltered;', (err, res) => {
+    db.run('SELECT * FROM tickets;', (err, res) => {
       if (err) callback(err, null);
       console.log(res.length + " items in db");
       callback(null, res);
@@ -13,7 +13,7 @@ module.exports = {
   },
   getCount(callback) {
     console.log('getCount');
-    db.ticketsunfiltered.count({}, (err, res) => {
+    db.tickets.count({}, (err, res) => {
       if (err) callback(err, null);
       console.log(res.length + " items in db");
       callback(null, res);
@@ -21,7 +21,7 @@ module.exports = {
   },
   getData(context, callback) {
     console.log('getData ', context);
-    db.ticketsunfiltered.find({'year>=':context.fromYear, 'year<=':context.toYear}, (err, res) => {
+    db.tickets.find({'year>=':context.fromYear, 'year<=':context.toYear}, (err, res) => {
       if (err) callback(err, null);
       console.log(res.length + " items found");
       callback(null, res);
